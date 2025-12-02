bd6abb007b5ccc54bbd3bf6f6236483a94c52e35
Kimutaijeremy <jrmkimutai@gmail.com>
2025-12-02 12:01:44 +0300

    Fileio lab

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,16 @@
 def write_file(file_name, file_content):
-    pass
+    """Write content to a .txt file"""
+    with open(f"{file_name}.txt", "w") as file:
+        file.write(file_content)
+
 
 def append_file(file_name, append_content):
-    pass
+    """Append content to an existing .txt file"""
+    with open(f"{file_name}.txt", "a") as file:
+        file.write(append_content)
+
 
 def read_file(file_name):
-    pass
+    """Read and return content from a .txt file"""
+    with open(f"{file_name}.txt", "r") as file:
+        return file.read()
